8adf20207e4b233e211af36ba195a6cb6e2279e5
tahsinabrar <abrar.cste@gmail.com>
2014-07-09 14:19:36 +0600

    Grading using Mark=63

--- ifelse.php
+++ ifelse.php
@@ -1,32 +1,32 @@
 <?php
-$marks=73;
+$marks=63;
 if($marks>=80&&$marks<=100){
 	$grade= "A+";
-	echo $grade;
+	echo "Grade= ".$grade;
 }
 elseif($marks>=70&&$marks<=79){
 	$grade= "A";
-	echo $grade;
+	echo "Grade= ".$grade;
 }
 elseif($marks>=60&&$marks<=69){
 	$grade= "A-";
-	echo $grade;
+	echo "Grade= ".$grade;
 }
 elseif($marks>=50&&$marks<=59){
 	$grade= "B";
-	echo $grade;
+	echo "Grade= ".$grade;
 }
 elseif($marks>=40&&$marks<=49){
 	$grade= "C";
-	echo $grade;
+	echo "Grade= ".$grade;
 }
 elseif($marks>=33&&$marks<=39){
 	$grade= "D";
-	echo $grade;
+	echo "Grade= ".$grade;
 }
 elseif($marks>=0&&$marks<=32){
 	$grade= "F";
-	echo $grade;
+	echo "Grade= ".$grade;
 }
 else{
 	echo "Wrong input";
