123d5bce51661360c8c273e710be59e29ab71a60
tahsinabrar <abrar.cste@gmail.com>
2014-07-09 15:49:24 +0600

    Edited code

--- grade.php
+++ grade.php
@@ -1,30 +1,14 @@
 <?php
 	function getGrade($marks){
-		if($marks>=80&&$marks<=100){
+		if($marks>=70&&$marks<=100){
 			$grade= "A+";
 			echo "<h1>Grade= ".$grade."</h1>";
 		}
-		elseif($marks>=70&&$marks<=79){
+		elseif($marks>=30&&$marks<=69){
 			$grade= "A";
 			echo "<h1>Grade= ".$grade."</h1>";
 		}
-		elseif($marks>=60&&$marks<=69){
-			$grade= "A-";
-			echo "<h1>Grade= ".$grade."</h1>";
-		}
-		elseif($marks>=50&&$marks<=59){
-			$grade= "B";
-			echo "<h1>Grade= ".$grade."</h1>";
-		}
-		elseif($marks>=40&&$marks<=49){
-			$grade= "C";
-			echo "<h1>Grade= ".$grade."</h1>";
-		}
-		elseif($marks>=33&&$marks<=39){
-			$grade= "D";
-			echo "<h1>Grade= ".$grade."</h1>";
-		}
-		elseif($marks>=0&&$marks<=32){
+		elseif($marks>=0&&$marks<=29){
 			$grade= "F";
 			echo "<h1>Grade= ".$grade."</h1>";
 		}
