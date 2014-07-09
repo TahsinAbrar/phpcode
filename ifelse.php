6673b6b8426b1603750dd3d6ba85b3d62172e2d7
tahsinabrar <abrar.cste@gmail.com>
2014-07-09 14:44:06 +0600

    Grading system and checking Grade

--- ifelse.php
+++ ifelse.php
@@ -1,30 +1,30 @@
 <?php
 $marks=51;
-if($marks>79){
+if($marks>=80&&$marks<=100){
 	$grade= "A+";
 	echo "<h1>Grade= ".$grade."</h1>";
 }
-elseif($marks>69){
+elseif($marks>=70&&$marks<=79){
 	$grade= "A";
 	echo "<h1>Grade= ".$grade."</h1>";
 }
-elseif($marks>59){
+elseif($marks>=60&&$marks<=69){
 	$grade= "A-";
 	echo "<h1>Grade= ".$grade."</h1>";
 }
-elseif($marks>49){
+elseif($marks>=50&&$marks<=59){
 	$grade= "B";
 	echo "<h1>Grade= ".$grade."</h1>";
 }
-elseif($marks>39){
+elseif($marks>=40&&$marks<=49){
 	$grade= "C";
 	echo "<h1>Grade= ".$grade."</h1>";
 }
-elseif($marks>32){
+elseif($marks>=33&&$marks<=39){
 	$grade= "D";
 	echo "<h1>Grade= ".$grade."</h1>";
 }
-elseif($marks<32){
+elseif($marks>=0&&$marks<=32){
 	$grade= "F";
 	echo "<h1>Grade= ".$grade."</h1>";
 }
