14f4b7768649979788499d5911212348e5acfae6
tahsinabrar <abrar.cste@gmail.com>
2014-07-09 15:10:05 +0600

    clean code with comment

--- loop.php
+++ loop.php
@@ -1,10 +1,10 @@
 <?php
-	$start=	10;
-	$end=2200;
-	$sum=0;
+	$start=	10; // initial variable
+	$end=2200; // end variable
+	$sum=0; // summation variable initialization
 	while($start<=$end){
 	 $sum=$sum+$start;
-	 $start++;
+	 $start++; // start variable is incremented by 1
 	}
-	echo $sum;
+	echo $sum; // output the result
 ?>
