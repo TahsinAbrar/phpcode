154aff23ae1a35be349756eeb4e29ff485d79518
tahsinabrar <abrar.cste@gmail.com>
2014-07-09 15:56:07 +0600

    Efficient code with function

--- grade.php
+++ grade.php
@@ -2,19 +2,18 @@
 	function getGrade($marks){
 		if($marks>=70&&$marks<=100){
 			$grade= "A+";
-			echo "<h1>Grade= ".$grade."</h1>";
 		}
 		elseif($marks>=30&&$marks<=69){
 			$grade= "A";
-			echo "<h1>Grade= ".$grade."</h1>";
 		}
 		elseif($marks>=0&&$marks<=29){
 			$grade= "F";
-			echo "<h1>Grade= ".$grade."</h1>";
 		}
 		else{
 			echo "Wrong input";
 		}
+		return $grade;
 	}
-	getGrade(73);
+	$grade=	getGrade(43);
+	echo "<h1>Grade= ".$grade."</h1>";
 ?>
