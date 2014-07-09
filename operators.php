5968f32776b9c7229da0a511151d1a8bc1a63f91
tahsinabrar <abrar.cste@gmail.com>
2014-07-09 13:41:12 +0600

    Unary operator precedence testing

--- operators.php
+++ operators.php
@@ -21,4 +21,19 @@ echo --$a."<br/>"; //2
 echo $a.PHP_EOL; //2
 echo ++$a;
 
+//Unary operator precedence testing
+
+$a=4;
+$b=2;
+echo ++$a*($a++ + --$b);
+/*
+* First execution starts with first bracket. 
+* Initial value of a is 4 and b is 2.
+* So, at first echo ++$a*(4+1);
+* Then $a will be incremented because $a++;
+* Then  $a will again be pre-incremented in the statement.
+* Now echo 6*(4+1);
+* So, the result is 30.
+*/
+
 ?>
