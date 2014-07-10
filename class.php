8aa929f06c8deaceb871ced3c0ebd9f497adb298
tahsinabrar <abrar.cste@gmail.com>
2014-07-10 15:11:05 +0600

    basic class concept

---
+++ class.php
@@ -0,0 +1,17 @@
+<?php
+class User{
+    private function userAge(){
+        return "some value";
+    }    
+}
+class Person extends User{
+    public function getUserAge(){
+        return $this->userAge();
+    }
+}
+
+$person= new Person();
+echo $person->getUserAge();
+echo '<br/>';
+echo $person->userAge();
+?>
