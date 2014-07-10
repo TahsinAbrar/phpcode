9766291f6cef81069bcaa6d72c6b252fdf240203
tahsinabrar <abrar.cste@gmail.com>
2014-07-10 12:44:36 +0600

    Define & Accessing class method

--- oop.php
+++ oop.php
@@ -3,10 +3,16 @@
     class User{
         var $name= "FTFL";
         var $email = "";
+        
+        function getName(){
+            return "FTFL2";
+        }
     }
     // Instantiate an object from the class
     $user=new User();
 
     // var_dump($user); // Debug the variable
-    echo $user->name;
+    echo $user->name; // Accesing class property
+    
+    echo $user->getName(); // Accessing class method
 ?>
