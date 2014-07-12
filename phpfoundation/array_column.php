9d7b06bada2f51c73b3eafd7ea8482a084b78b97
Tahsin Abrar <explorer.tahsin@gmail.com>
2014-07-13 03:59:35 +0600

    Using array_column function
    Here, I tried to use the array_column function

---
+++ phpfoundation/array_column.php
@@ -0,0 +1,28 @@
+<?php
+// Array representing a possible record set returned from a database
+$records = array(
+    array(
+        'id' => 2135,
+        'first_name' => 'John',
+        'last_name' => 'Doe',
+    ),
+    array(
+        'id' => 3245,
+        'first_name' => 'Sally',
+        'last_name' => 'Smith',
+    ),
+    array(
+        'id' => 5342,
+        'first_name' => 'Jane',
+        'last_name' => 'Jones',
+    ),
+    array(
+        'id' => 5623,
+        'first_name' => 'Peter',
+        'last_name' => 'Doe',
+    )
+);
+ 
+$last_names = array_column($records, 'last_name', 'id');
+print_r($last_names);
+?>
