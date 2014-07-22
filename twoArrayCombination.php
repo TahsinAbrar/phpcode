c6b275d51f6a24290577fcab3c4749c6a0db19bf
Tahsin Abrar <abrar.cste@gmail.com>
2014-07-22 23:14:08 +0600

    storing output within result array

--- twoArrayCombination.php
+++ twoArrayCombination.php
@@ -1,13 +1,16 @@
 <?php
 
-$data_one=array('A'=>'apple','B'=>'ball');
+$data_one=array('A'=>'Apple','B'=>'Ball');
 $data_two=array('C'=>'Cat','D'=>'Dog','E'=>'Egg');
+$count=0;
 foreach ($data_one as $key1 => $value1) {
     foreach ($data_two as $key2 => $value2) {
-        echo $key1.'-'.$key2.' : '.$value1.'-'.$value2."<br/>";
-    }
-    
+        $result[] = $key1.'-'.$key2.' : '.$value1.'-'.$value2."<br/>";
+        echo $result[$count];
+        $count++;
+    }   
 }
+
 
 
 ?>
